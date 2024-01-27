6c5c7ddb83467c400b5afdcfd1ff1cb3f6913cdc
Shiyu Zhou <szho231@aucklanduni.ac.nz>
2024-01-27 13:14:04 +1300

    chakra installed

---
+++ Setting_Up_the_Project.ts
@@ -0,0 +1,17 @@
+/** Setting Up
+ * in the terminal -- npm create vite@4.1.0
+ * choose React, then choose typeScript
+ * enter Project Name
+ * direct to the folder -- npm i, then npm run dev
+ *
+ * git init
+ * git add .
+ * git commit -m "Initial commit"
+ */
+
+/** Installing Chakra UI
+ * https://chakra-ui.com/getting-started
+ * npm i @chakra-ui/react @emotion/react @emotion/styled framer-motion
+ * inside main.tsx -- import { ChakraProvider } from '@chakra-ui/react'
+ * wrap app with <ChakraProvider> </ChakraProvider>
+ */
