59c7a3b1b7a9949faad888d6df3fc5b958834237
Shiyu Zhou <szho231@aucklanduni.ac.nz>
2024-01-27 13:33:33 +1300

    Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  /** 5-Creating a Responsive Layout*/
+
+  // Default breakpoints:
+  // const breakpoints = {
+  //   base: "0em", // 0px
+  //   sm: "30em", // ~480px. em is a relative unit and is dependant on the font size.
+  //   md: "48em", // ~768px
+  //   lg: "62em", // ~992px
+  //   xl: "80em", // ~1280px
+  //   "2xl": "96em", // ~1536px
+  // };
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" " aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
