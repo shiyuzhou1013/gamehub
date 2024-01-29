bd62e50f70e2f9bf1505879ea6dda67e42c304c4
Shiyu Zhou <szho231@aucklanduni.ac.nz>
2024-01-29 16:33:46 +1300

    spinner

--- src/components/17-GenreList.tsx
+++ src/components/17-GenreList.tsx
@@ -1,9 +1,14 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/17-useGenres";
 import getCroppedImageUrl from "../services/14-image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+
+  if (isLoading) return <Spinner />;
+
   return (
     <List>
       {data.map((genre) => (
