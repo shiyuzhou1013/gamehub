5c1569080076adc136ba95e292c97d9fd8aa7bea
Shiyu Zhou <szho231@aucklanduni.ac.nz>
2024-01-31 14:20:26 +1300

    no image handled

--- src/services/14-image-url.ts
+++ src/services/14-image-url.ts
@@ -1,5 +1,8 @@
+/** 28-Handing Games without an Image */
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
 
   const target = "media/";
   const index = url.indexOf(target) + target.length;
