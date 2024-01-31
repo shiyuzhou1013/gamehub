9ef79907cec0ee1138dcecc052ea5112e5a4973c
Shiyu Zhou <szho231@aucklanduni.ac.nz>
2024-01-31 20:31:00 +1300

    search bar built

--- src/components/6-NavBar.tsx
+++ src/components/6-NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./8-ColorModeSwitch";
+import SearchInput from "./30-SearchInput";
 
 const NavBar = () => {
   //HStack -- horizontal stack
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
