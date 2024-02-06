d4bf52af4974415160a2b957bf9d8dec87c124a6
Shiyu Zhou <szho231@aucklanduni.ac.nz>
2024-02-07 10:10:49 +1300

    fetching platforms improved

--- src/hooks/23-usePlatforms.ts
+++ src/hooks/23-usePlatforms.ts
@@ -1,6 +1,20 @@
+import platforms from "../data/36-platforms";
+import { useQuery } from "@tanstack/react-query";
 import { Platform } from "./10-useGames";
-import useData from "./18-useData";
+import { FetchResponse } from "./18-useData";
+import apiClient from "../services/9-api-client";
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+/** Advanced - 25-Exercise-Fetching Platforms */
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24 hours
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
