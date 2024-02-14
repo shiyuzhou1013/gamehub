e1df0fed2daa73519afdc0c5b6dcb58beda6fbfb
Shiyu Zhou <szho231@aucklanduni.ac.nz>
2024-02-15 11:15:48 +1300

    making text expandable

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/15(Part3)-useGams";
 import { Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/17(Part3)-ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -13,7 +14,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
